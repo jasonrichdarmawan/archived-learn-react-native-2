4b7beb6323182143b1e2ab2b25159422b0a4786e
Jason <jason.onggo@tempatkerja.com>
2020-12-07 20:42:29 +0700

    add react-native-bluetooth-classic

--- record_audio_expo/App.tsx
+++ record_audio_expo/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {
+  PermissionsAndroid,
+  Platform,
   Pressable,
   SafeAreaView,
   ScrollView,
@@ -9,7 +11,8 @@ import {
 } from "react-native";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 import { Audio } from "expo-av";
-import * as FileSystem from "expo-file-system";
+// import * as FileSystem from "expo-file-system";
+import RNBluetoothClassic, { BTDevice } from "react-native-bluetooth-classic";
 
 export default function App() {
   let recorder = new Audio.Recording();
@@ -127,6 +130,62 @@ export default function App() {
     }
   };
 
+  const bluetoothIsEnabled = async () => {
+    try {
+      const enabled = await RNBluetoothClassic.isEnabled();
+      console.log(`bluetooth is enabled: ${enabled}`);
+      return enabled;
+    } catch (error) {
+      console.error(`bluetoothIsEnabled(): ${error}`);
+      return false;
+    }
+  };
+
+  /**
+   * // TODO: fix bluetooth is connected although bluetooth is not enabled.
+   * Steps to reproduce: Connect to a bluetooth device -> disable the bluetooth -> use method bluetoothIsConnected();
+   * Severity: Major
+   */
+  const bluetoothIsConnected = async () => {
+    try {
+      const connected = await RNBluetoothClassic.isConnected();
+      console.log(`bluetooth is connected: ${connected}`);
+      return connected;
+    } catch (error) {
+      console.error(`bluetoothIsConnected(): ${error}`);
+      return false;
+    }
+  };
+
+  let bluetoothDeviceID = "00:13:04:84:03:07"; // TODO: let the user choose.
+
+  const bluetoothConnect = async (deviceID: string) => {
+    const enabled = await bluetoothIsEnabled();
+    if (enabled === true) {
+      try {
+        const connected = await bluetoothIsConnected();
+        if (connected === false) {
+          await RNBluetoothClassic.connect(deviceID);
+          const result = await bluetoothIsConnected();
+          console.log(
+            `connected to bluetooth device with device id '${deviceID}': ${result}`
+          );
+        }
+      } catch (error) {
+        console.error(`bluetoothConnect(): ${error}`);
+      }
+    }
+  };
+
+  const bluetoothList = async () => {
+    try {
+      const list = await RNBluetoothClassic.list();
+      console.log(list);
+    } catch (error) {
+      console.error(`bluetoothList(): ${error}`);
+    }
+  };
+
   React.useEffect(() => {
     console.log("re-render");
   });
@@ -146,7 +205,7 @@ export default function App() {
           <View style={styles.sectionContainer}>
             <Pressable
               onPress={() => {
-                if (uri != null) {
+                if (uri !== null) {
                   playerPlay(uri);
                 }
               }}
@@ -162,6 +221,22 @@ export default function App() {
           <View style={styles.sectionContainer}>
             <Pressable onPress={() => playerUnpause()}>
               <Text>Unpause the audio</Text>
+            </Pressable>
+          </View>
+          <View style={styles.sectionContainer}>
+            <Pressable onPress={() => bluetoothList()}>
+              <Text>Bluetooth List</Text>
+            </Pressable>
+          </View>
+          <View style={styles.sectionContainer}>
+            <Pressable
+              onPress={() => {
+                if (bluetoothDeviceID !== null) {
+                  bluetoothConnect(bluetoothDeviceID);
+                }
+              }}
+            >
+              <Text>Bluetooth Connect</Text>
             </Pressable>
           </View>
         </View>
