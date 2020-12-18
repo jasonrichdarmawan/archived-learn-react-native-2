53bf7de5b797aba56a52b1d4881595ea4ebd95e8
Jason <jason.onggo@tempatkerja.com>
2020-12-19 01:44:51 +0700

    add typescript

---
+++ ws_send_audio/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Pressable, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Pressable>
+        <Text>Record</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default App;
