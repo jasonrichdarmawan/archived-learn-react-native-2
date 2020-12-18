36ea297ef97cdfaff8a482fd5da98e35b47e3951
Jason <jason.onggo@tempatkerja.com>
2020-12-19 06:49:59 +0700

    yarn add sockjs-client and stompjs

--- ws_send_audio/App.tsx
+++ ws_send_audio/App.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import {View, Pressable, Text} from 'react-native';
+import {View, Pressable, Text, PermissionsAndroid} from 'react-native';
+import Recording from 'react-native-recording';
 
 const App = () => {
+  React.useEffect(() => {
+    const listener = Recording.addRecordingEventListener((data) =>
+      console.log(data),
+    );
+    return () => listener.remove();
+  });
+
+  const startAsync = async () => {
+    await PermissionsAndroid.requestMultiple([
+      PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+    ]);
+
+    Recording.init({
+      bufferSize: 4096,
+      sampleRate: 44100,
+      bitsPerChannel: 16,
+      channelsPerFrame: 1,
+    });
+
+    Recording.start();
+  };
+
   return (
     <View>
-      <Pressable>
+      <Pressable
+        onPressIn={() => startAsync()}
+        onPressOut={() => Recording.stop()}>
         <Text>Record</Text>
       </Pressable>
     </View>
