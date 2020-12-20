33dfe2d3a4509d7517b6054a6e679dba7b48bb16
Jason <jason.onggo@tempatkerja.com>
2020-12-20 23:03:48 +0700

    example without SockJs

--- ws_send_audio/App.tsx
+++ ws_send_audio/App.tsx
@@ -1,13 +1,57 @@
 import React from 'react';
 import {View, Pressable, Text, PermissionsAndroid} from 'react-native';
 import Recording from 'react-native-recording';
+import {Client} from '@stomp/stompjs';
 
 const App = () => {
   React.useEffect(() => {
+    const stompClient = new Client({
+      brokerURL: 'ws://192.168.1.8:8080/websocketApp',
+      // debug: (str) => {
+      //   console.log(str);
+      // },
+      forceBinaryWSFrames: true,
+      appendMissingNULLonIncoming: true,
+    });
+
+    let stompSubscription;
+
+    stompClient.onConnect = (frame) => {
+      console.log('onConnect');
+      stompSubscription = stompClient.subscribe(
+        '/topic/javainuse',
+        (message) => {
+          if (message.body) {
+            console.log('got message with body ' + message.body);
+          } else {
+            console.log('got empty message');
+          }
+        },
+      );
+      stompClient.publish({
+        destination: '/app/chat.newUser',
+        body: JSON.stringify({
+          sender: 'User A',
+          type: 'newUser',
+        }),
+        skipContentLengthHeader: true,
+      });
+    };
+
+    stompClient.onStompError = (frame) => {
+      console.log('onStompError');
+    };
+
+    stompClient.activate();
+
     const listener = Recording.addRecordingEventListener((data) =>
       console.log(data),
     );
-    return () => listener.remove();
+
+    return () => {
+      listener.remove();
+      stompSubscription.unsubscribe();
+    };
   });
 
   const startAsync = async () => {
